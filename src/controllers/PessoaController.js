00c222967a405f83f7ecdd7daab901dab458a7d7
carloseocandido <carloseocandido@gmail.com>
2023-09-04 17:01:11 -0300

    crud pessoas

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -1,12 +1,59 @@
 const database = require('../models')
 
 class PessoaController {
-    static async listarTodasAsPessoas(req, res) {
+    static listarTodasAsPessoas = async (req, res) => {
         try {
-            const todasAsPessoas = await database.Pessoas.findAll()
-            return res.status(200).json(todasAsPessoas)
+            const pessoasResultado = await database.Pessoas.findAll()
+            return res.status(200).json(pessoasResultado)
         } catch (error) {
             return res.status(500).json(error.message)
+        }
+    }
+
+    static listarPorId = async (req, res) => {
+        const { id } = req.params;
+
+        try {
+            const pessoasResultado = await database.Pessoas.findOne({ where: { id: Number(id) } });
+            return res.status(200).json(pessoasResultado);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static cadastrarPessoas = async (req, res) => {
+        const resultado = req.body
+        try {
+            const pessoasResultado = await database.Pessoas.create(resultado)
+            return res.status(200).json(pessoasResultado)
+
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static atualizarPessoas = async (req, res) => {
+        const { id } = req.params
+        const resultado = req.body
+
+        try {
+            await database.Pessoas.update(resultado, { where: { id: Number(id) } })
+            const pessoasAtualizada = await database.Pessoas.findOne({ where: { id: Number(id) } });
+
+            res.status(200).json(pessoasAtualizada)
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static apagarPessoas = async (req, res) => {
+        const { id } = req.params
+
+        try {
+            await database.Pessoas.destroy({ where: { id: Number(id) } })
+            return res.status(200).json({ message: `O registro ${id} foi excluido.` })
+        } catch (error) {
+            return res.status(500).json(error.message);
         }
     }
 }
--- src/routes/pessoasRoute.js
+++ src/routes/pessoasRoute.js
@@ -5,5 +5,9 @@ const router = Router()
 
 router
     .get('/pessoas', PessoaController.listarTodasAsPessoas)
+    .get('/pessoas/:id', PessoaController.listarPorId)
+    .post('/pessoas', PessoaController.cadastrarPessoas)
+    .put('/pessoas/:id', PessoaController.atualizarPessoas)
+    .delete('/pessoas/:id', PessoaController.apagarPessoas)
 
 module.exports = router
